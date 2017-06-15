98250b45b7703d3b4f3161e1f3f36134ddcfc360
Meliton G. Lazaro <melitonlazaro1@gmail.com>
2017-06-15 17:36:30 +0800

    modified table

--- application/views/daily_log.php
+++ application/views/daily_log.php
@@ -23,27 +23,59 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<tr>
 		<th>ID</th>
 		<th>Employee Name</th>
+		<th>Department</th>
 		<th>Issues</th>
 		<th>Remarks</th>
 		<th>Status</th>
 		<th>Tech</th>
+		<th>Date</th>
+		<th>Action</th>
 	</tr>
 
 </thead>
 <tbody>
 <?php
 foreach($log as $logs):
-
-	echo "<tr>";
-	echo "<td>$logs->id</td>";
-	echo "<td>$logs->employee</td>";
-	echo "<td>$logs->conflict</td>";
-	echo "<td>$logs->remarks</td>";
-	echo "<td>$logs->status</td>";
-	echo "<td>$logs->tech</td>";
-	echo "<td>$logs->date</td>";
-	echo "</tr>";
-
+	if($logs->status === "Complete")
+		{
+		echo "<tr class='success'>";
+		echo "<td>$logs->id</td>";
+		echo "<td>$logs->employee</td>";
+		echo "<td>$logs->department</td>";
+		echo "<td>$logs->conflict</td>";
+		echo "<td>$logs->remarks</td>";
+		echo "<td>$logs->status</td>";
+		echo "<td>$logs->tech</td>";
+		echo "<td>$logs->date</td>";
+		echo "</tr>";
+		}
+	if($logs->status === "Pending")
+		{
+		echo "<tr class='warning'>";
+		echo "<td>$logs->id</td>";
+		echo "<td>$logs->employee</td>";
+		echo "<td>$logs->department</td>";
+		echo "<td>$logs->conflict</td>";
+		echo "<td>$logs->remarks</td>";
+		echo "<td>$logs->status</td>";
+		echo "<td>$logs->tech</td>";
+		echo "<td>$logs->date</td>";
+		echo "</tr>";
+		}	
+	if($logs->status === "Incomplete")
+		{
+		echo "<tr class='danger'>";
+		echo "<td>$logs->id</td>";
+		echo "<td>$logs->employee</td>";
+		echo "<td>$logs->department</td>";
+		echo "<td>$logs->conflict</td>";
+		echo "<td>$logs->remarks</td>";
+		echo "<td>$logs->status</td>";
+		echo "<td>$logs->tech</td>";
+		echo "<td>$logs->date</td>";
+		echo "</tr>";
+		}
+	
 ?>
 <?php endforeach;?>
 
